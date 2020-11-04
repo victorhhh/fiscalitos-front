26a5b5f1f24a06d7c616d59a0eac16fe8bf805ba
Víctor <victor_hdez96@hotmail.com>
2020-11-04 17:43:05 -0600

    Add formulario login

---
+++ src/components/Modal.vue
@@ -0,0 +1,114 @@
+<template>
+  <v-dialog
+    persistent
+    v-model="abrido"
+    max-width="500px"
+    transition="dialog-transition"
+  >
+    <v-card>
+      <v-container fill-height>
+        <v-row>
+          <v-col>
+            <v-card-title> <h1>Login</h1> </v-card-title>
+            <template>
+              <v-form v-model="valid" ref="form" lazy-validation>
+                <v-text-field
+                  rounded
+                  outlined
+                  label="Nickname"
+                  v-model="name"
+                  :rules="nameRules"
+                  :counter="10"
+                  required
+                ></v-text-field>
+                <v-text-field
+                  rounded
+                  outlined
+                  label="E-mail"
+                  v-model="email"
+                  :rules="emailRules"
+                  required
+                ></v-text-field>
+                <v-text-field
+                  :type="text? 'text': 'password'"
+                  error-count="10"
+                  label="Password"
+                  :append-icon="text?'mdi-eye-off':'mdi-eye'"
+                  @click:append="() => (text = !text)"
+                  v-model="password"
+                  outlined
+                  rounded
+                ></v-text-field>
+
+                <v-checkbox
+                  label="Do you agree?"
+                  v-model="checkbox"
+                  :rules="[(v) => !!v || 'You must agree to continue!']"
+                  required
+                ></v-checkbox>
+                <v-card-actions>
+                  <v-btn @click="submit" :disabled="!valid"> submit </v-btn>
+                  <v-btn @click="clear">clear</v-btn>
+
+                  <v-spacer></v-spacer
+                  ><v-btn color="primary" @click="$emit('cerrar', false)"
+                    >Close</v-btn
+                  >
+                </v-card-actions>
+              </v-form>
+            </template>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  name: "Modal",
+  props: {
+    abrido: Boolean,
+  },
+  data: () => ({
+    text: false,
+    valid: true,
+    name: "a",
+    nameRules: [
+      (v) => !!v || "Nickname is required",
+      (v) => (v && v.length <= 10) || "Name must be less than 10 characters",
+    ],
+    email: "a@a.aa",
+    emailRules: [
+      (v) => !!v || "E-mail is required",
+      (v) =>
+        /^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/.test(
+          v
+        ) || "E-mail must be valid",
+    ],
+    password: "a",
+    checkbox: true,
+  }),
+  methods: {
+  async submit() {
+      if (this.$refs.form.validate()) {
+        // Native form submission is not yet supported
+        // axios.post("/api/submit", {
+        //   name: this.name,
+        //   email: this.email,
+        //   select: this.select,
+        //   checkbox: this.checkbox,
+        // });
+        await fetch("127.0.0.1:3000/hola").then(response=>{console.log(response.body)})
+        console.log("Holiii");
+      }
+    },
+    clear() {
+      this.$refs.form.reset();
+    },
+  },
+};
+</script>
+
+<style>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,54 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-container fill-height>
+    <v-row class="d-flex justify-center">
+      <v-col cols="6">
+        <v-card elevation="24" min-height="200">
+          <v-container fill-height>
+            <v-row>
+              <v-col>
+                <v-row>
+                  <v-col class="d-flex justify-center">
+                    <h1>
+                      Fiscalgram
+                      <span> <v-icon large>mdi-instagram</v-icon> </span>
+                    </h1>
+                  </v-col>
+                </v-row>
+                <v-row
+                  ><v-col class="d-flex justify-center"
+                    ><v-btn color="primary" @click="abrido = !abrido"
+                      >Entrar</v-btn
+                    >
+                    <Modal
+                      :abrido="abrido"
+                      @cerrar="abrido = $event"
+                    ></Modal> </v-col
+                ></v-row>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
+import Modal from "@/components/Modal.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    Modal,
+    HelloWorld,
+  },
+  data: () => ({
+    formulario: null,
+    abrido: false,
+    form: 1,
+  }),
+};
 </script>
